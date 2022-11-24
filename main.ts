a5a4b91b46d0a9e3470fe52775ffdd1886da03fb
Monicanl <118841496+Monicanl@users.noreply.github.com>
2022-11-24 17:58:50 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,27 @@
+let B = 0
+let G = 0
+let R = 0
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
-    basic.pause(1000)
+    R = 0
+    G = 0
+    B = 0
+    for (let index = 0; index < 255; index++) {
+        R += 1
+        B += -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 255; index++) {
+        G += 1
+        R += -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 255; index++) {
+        B += 1
+        G += -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1000)
+    }
 })
